b6352375457cdf46bf4a9d28c8b9c8dc8cf68278
DrakSkoldpadda <42768824+DrakSkoldpadda@users.noreply.github.com>
2019-09-19 12:26:20 +0200

    enemy

---
+++ Grupp2DigDig/Assets/Scripts/Enemy/EnemyStunn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStunn : MonoBehaviour
+{
+    [SerializeField] private float stunnDuration = 0.7f;
+    private float stunnTimeLeft;
+
+    private Pathfiding AI;
+
+    private void Awake()
+    {
+        AI = GetComponentInParent<Pathfiding>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            stunnTimeLeft = stunnDuration;
+        }
+    }
+
+    private void Update()
+    {
+        if (stunnTimeLeft > 0)
+        {
+            AI.agent.isStopped = true;
+            stunnTimeLeft -= Time.deltaTime;
+        }
+        else
+        {
+            AI.agent.isStopped = false;
+        }
+    }
+}
