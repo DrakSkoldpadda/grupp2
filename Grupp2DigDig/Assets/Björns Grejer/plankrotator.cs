83e941e4461bd55717753e6c7b961840be3b06ce
Bullen <42798106+FUSLUTA@users.noreply.github.com>
2019-10-24 11:52:30 +0200

    pokmrowagetrporgiogjreoigjeprjge
    oojojo

---
+++ Grupp2DigDig/Assets/Björns Grejer/plankrotator.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plankrotator : MonoBehaviour
+{
+    public float speed;
+
+    bool ready;
+
+    private void Start()
+    {
+        ready = true;
+    }
+
+    public int stopNumber;
+    int counter;
+    IEnumerator RotateToPLayer()
+    {
+        print("Forwardstime");
+        yield return new WaitForFixedUpdate();
+        transform.Rotate(0, speed, 0);
+
+        counter++;
+        if (stopNumber > counter)
+        {
+            StartCoroutine(RotateToPLayer());
+        }
+        else
+        {
+            StopCoroutine(RotateToPLayer());
+            ready = true;
+        }
+    }
+
+    IEnumerator RotateToPLayerBackwards()
+    {
+        print("backwardstime");
+        yield return new WaitForFixedUpdate();
+        transform.Rotate(0, -speed, 0);
+
+        counter++;
+        if (-stopNumber > counter)
+        {
+            StartCoroutine(RotateToPLayerBackwards());
+        }
+        else
+        {
+            StopCoroutine(RotateToPLayerBackwards());
+            ready = true;
+        }
+    }
+
+    public void RotateHelper(bool ForwardTrue)
+    {
+        if (ready == true)
+            if (ForwardTrue)
+            {
+                StartCoroutine(RotateToPLayer());
+                ready = false;
+            }
+            else
+            {
+                StartCoroutine(RotateToPLayerBackwards());
+                ready = false;
+            }
+    }
+}
