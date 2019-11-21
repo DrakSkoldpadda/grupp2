bb5e65e22668ae48f03eed692f4edb027241be03
DrakSkoldpadda <42768824+DrakSkoldpadda@users.noreply.github.com>
2019-11-21 11:53:06 +0100

    brap

--- Grupp2DigDig/Assets/Scripts/UI/FPS.cs
+++ Grupp2DigDig/Assets/Scripts/UI/FPS.cs
@@ -6,6 +6,11 @@ public class FPS : MonoBehaviour
 {
     float deltaTime = 0.0f;
 
+    void Start()
+    {
+        Application.targetFrameRate = 300;
+    }
+
     void Update()
     {
         deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
