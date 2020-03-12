3572cb6e2f931ba2ff81028705eabe9ccf252619
SweKing <fknripmobil@gmail.com>
2020-03-12 10:53:12 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHH

---
+++ Grupp2DigDig/Assets/LeafFallScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeafFallScript : MonoBehaviour
+{
+    Transform PlayerTransform;
+    public float distance;
+    private ParticleSystem PS;
+
+    private void Start()
+    {
+        PS = GetComponentInChildren<ParticleSystem>();
+        PlayerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        StartCoroutine(Swag());
+    }
+
+
+    public IEnumerator Swag()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        float distanceToPlayer = Vector3.Distance(PlayerTransform.position, transform.position);
+
+        if (distanceToPlayer > distance)
+        {
+            PS.Stop();
+        }
+
+        else
+            PS.Play();
+
+
+        StartCoroutine(Swag());
+    }
+}
