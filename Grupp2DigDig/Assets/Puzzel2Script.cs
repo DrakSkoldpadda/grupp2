25d0f89d3acc0bbf19d2a18f80814b76b877bfcf
Bullen <42798106+FUSLUTA@users.noreply.github.com>
2020-04-16 11:18:24 +0200

    dfweg
    dferge

---
+++ Grupp2DigDig/Assets/Puzzel2Script.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzel2Script : MonoBehaviour
+{
+
+    public GameObject hingeJärn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Swagmoney()
+    {
+        yield return new WaitForSeconds(3);
+
+        transform.Rotate(hingeJärn.transform.up, 90);
+    }
+}
