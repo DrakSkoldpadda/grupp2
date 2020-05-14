e5b4429bad8e5f36dfcf6a7beb5555685c5dd554
Bullen <42798106+FUSLUTA@users.noreply.github.com>
2020-05-14 15:19:23 +0200

    dgshrtjkyugikh

---
+++ Grupp2DigDig/Assets/svampLjud.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class svampLjud : MonoBehaviour
+{
+    public AudioClip[] ljud;
+
+    public AudioSource source;
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            source.clip = ljud[Random.Range(0, 6)];
+
+            source.Play();
+        }
+    }
+}
